0d019cf6061a00851da95925e88b2bcd36a2e19b
KxllSwxtch <dimashin9@gmail.com>
2025-03-18 16:47:29 +0900

    updates

--- main.py
+++ main.py
@@ -426,7 +426,6 @@ def calculate_cost(link, message):
         try:
             response = requests.get(
                 new_url,
-                proxies=proxy,
                 headers={
                     "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"
                 },
